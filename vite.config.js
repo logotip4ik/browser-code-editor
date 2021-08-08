a840a2ee7f5f46a23714daa1b38d14e826bda9fa
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-08-08 22:41:16 +0300

    update: added pwa for working offline

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,20 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-// import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA } from "vite-plugin-pwa";
 
-// VitePWA({
-// 	registerType: "autoUpdate",
-// 	manifest: {
-// 		name: "Browser Code Editor",
-// 		short_name: "BCE",
-// 		start_url: ".",
-// 		display: "fullscreen",
-// 		background_color: "#292A2B",
-// 		description: "Simple Browser code editor + PWA for working offline!",
-// 	},
-// });
+VitePWA({
+	registerType: "autoUpdate",
+	manifest: {
+		name: "Browser Code Editor",
+		short_name: "BCE",
+		start_url: ".",
+		display: "fullscreen",
+		background_color: "#292A2B",
+		description: "Simple Browser code editor + PWA for working offline!",
+	},
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		vue(),
-		// VitePWA()
-	],
+	plugins: [vue(), VitePWA()],
 });
